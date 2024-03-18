63b1892b650dba61631855416aee795d3975a70d
arnold <arnoldkmaina@gmail.com>
2024-03-18 20:26:27 +0300

    reviews string labs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+const currentUser = 'Arnold';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+'Edsger Dijkstra'.charAt(0);
+'Edsger Dijkstra'.length;
+'Edsger Dijkstra'[14];
+'Edsger Dijkstra'.slice(7);
+'Edsger Dijkstra'.slice(0, 3);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
